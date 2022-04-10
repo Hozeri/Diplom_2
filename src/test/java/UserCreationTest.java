01814c4017ea60202702d183cb243ee37a2452f1
Aleksey Karev <laurent.94@yandex.ru>
2022-04-10 14:25:38 +0300

    Optimized imports in the UserCreationTest

--- src/test/java/UserCreationTest.java
+++ src/test/java/UserCreationTest.java
@@ -6,7 +6,8 @@ import org.junit.Test;
 
 import static org.apache.http.HttpStatus.SC_OK;
 import static org.apache.http.HttpStatus.SC_FORBIDDEN;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class UserCreationTest {
 
